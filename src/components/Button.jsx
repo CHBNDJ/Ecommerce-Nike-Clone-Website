f5e73674b2d1d04261d3debe5872b78bb79afcc8
Chabane Nadji <chabane.nadji@gmail.com>
2025-08-02 09:42:04 +0200

    Section Services Created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,11 +2,13 @@ const Button = ({ label, iconURL }) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-red-400 rounded-full text-white border-red-400">
       {label}
-      <img
-        src={iconURL}
-        alt="right arrow"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="right arrow"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,3 +1,6 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
     <section
@@ -7,14 +10,29 @@ const SuperQuality = () => {
       <div className="flex flex-col flex-1">
         <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
           We Provide You
-          <span className="text-red-400">Super</span>
-          <span className="text-red-400">Quality </span> Shoes
+          <span className="text-red-400"> Super</span>
+          <span className="text-red-400"> Quality </span> Shoes
         </h2>
-        <p className="font-montserrat text-slate-500 text-lg mt-6 mb-14 sm:max-w-sm">
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
-        <Button label="View Details" iconURL={arrowRight} />
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+      <div className="flex flex-1 justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
       </div>
     </section>
   );
